d3d575e9ca574c3ce8765852b57e49cfd2a1a6a9
Matthew Howell <mrhowel@g.clemson.edu>
2019-08-15 00:43:34 -0400

    More class experimentation

--- main_classes2.py
+++ main_classes2.py
@@ -1,17 +1,63 @@
-class ServiceNow:
-    class SearchPage:
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions as seleniumExceptions
+import selenium.webdriver.support.select
+import traceback
+import shutil
+import datetime
+import os
+import argparse
+import time
+import csv
+
+driver = webdriver.Chrome()
+
+class CatalogTasksPage:
+
+    def __init__(self):
         pass
 
-        class RequestItems:
-            def getRequestItem(self):
-                return RequestItem
+    def findRequestItem(self,cond):
+        print(str(cond))
+        #Create table. Search and filter table
+        req = RequestItem()
+        return req
+
+class IncidentsPage:
+    pass
 
 
-        class CatalogTasks:
-            pass
+class RequestedItemsPage:
+    pass
 
-        class Incidents:
-            pass
+class SearchPage:
+
+    def clickCatalogTasks(self):
+        cat = CatalogTasksPage()
+        return cat
+
+    def clickIncidents(self):
+        incident = IncidentsPage()
+        return incident
+
+    def clickRequestedItems(self):
+        requestedItems = RequestedItemsPage()
+        return requestedItems
+
+class ServiceNow:
+
+    def searchFor(self, string): # Need to use composition or inheritance so everything can access global ServiceNow functionality
+        print("Searching for " + string)
+        searchbox = driver.find_element_by_xpath("//*[@id='sysparm_search']")
+        searchbox.clear()
+        searchbox.click()
+        searchbox.send_keys(string)
+        searchbox.send_keys(Keys.RETURN)
+        results = SearchPage()
+        return results
 
     class SelfService:
         class DesktopSupport:
@@ -24,7 +70,7 @@ class ServiceNow:
 class RequestItem:
     state = None
     number = None
-    item = None
+    item = 'yo'
     request = None
     requestedBy = None
     dueDate = None
@@ -40,4 +86,4 @@ class RequestItem:
         pass
 
 serviceNow = ServiceNow()
-serviceNow.SearchPage.RequestItems.getRequestItem()
+serviceNow.searchFor('123').clickCatalogTasks().findRequestItem(['conditions'])
