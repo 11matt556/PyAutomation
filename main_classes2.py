3fa43736bfd6b092f2f84adfd482b08ddc9c388d
Matthew Howell <mrhowel@g.clemson.edu>
2019-09-20 22:15:57 -0400

    Import CSV. Now reading more than just first column. In theory everything is in place to complete restock tickets

--- main_classes2.py
+++ main_classes2.py
@@ -13,6 +13,22 @@ import argparse
 import time
 import csv
 
+__CANNED_RESPONSES = {
+    "acknowledge":      "Acknowledging asset/ticket",
+    "restock":          "Device to be restocked, W10 Rollout; Device has been tested and wiped",
+    "reclaim":          "Device has been reclaimed; Device to be restocked after testing & cleaning.",
+    "decommission":     "This device is being decommissioned; send to MDC",
+    "repair_mdc":       "Device to be sent in for repair/stock at MDC",
+    "repair_isc_ssd":   "USDT device to be repaired with SSD swapout"
+}
+
+__VALID_STATES = {
+    "wip": "Work in Progress",
+    "cc": "Closed Complete"
+}
+
+SAVE_TICKET = False
+
 driver = webdriver.Chrome()
 
 def switchToContentFrame():
@@ -23,19 +39,65 @@ def switchToDefaultFrame():
     driver.switch_to.default_content()
 
 
-class Notes:
+class CSV:
+    @staticmethod
+    def import_csv(inputCSV):
+        print("Importing items...")
+        with open(inputCSV) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            outputArray = []
+            for row in csv_reader:
+                outputArray.append(row)
+                print("Importing line: " + str(row))
+                line_count += 1
 
-    def setAdditionalComments(self, commentString):
+            print(f'Imported {line_count} items')
+            print("Result: " + str(outputArray))
+            return outputArray
+
+
+class CatalogTask:
+    notes_tab = None
+    details_tab = None
+    variables_tab = None
+
+    def __init__(self):
         switchToDefaultFrame()
         switchToContentFrame()
-        print("Typing " + str(commentString))
-        commentArea = driver.find_element_by_xpath("//textarea[@id='activity-stream-comments-textarea']")
-        # Sometimes the comment box area is collapsed by default, so we must click the button to expand it first
-        try:
-            commentArea.send_keys(commentString)
-        except seleniumExceptions.ElementNotInteractableException:
-            driver.find_element_by_xpath("//span[2]/span/nav/div/div[2]/span/button").click()
-            commentArea.send_keys(commentString)
+        self.notes_tab = Notes()
+        self.details_tab = Details()
+        self.number = driver.find_element_by_id("sc_task.number").get_attribute("value")
+        self.requestItem = driver.find_element_by_id("sys_display.sc_task.request_item").get_attribute("value")
+        self.shortDescription = driver.find_element_by_id("sc_task.short_description").get_attribute("value")
+        self.state = select.Select(driver.find_element_by_id('sc_task.state'))
+        self.assignedTo = driver.find_element_by_id('sys_display.sc_task.assigned_to').get_attribute("value")
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self, state):
+        print("Setting ticket state to " + state)
+        driver.find_element_by_id('sc_task.state').find_element_by_xpath("//option[. = '" + state + "']").click()
+        self.state = state
+
+    def submit(self):
+        actions = webdriver.ActionChains(driver)
+
+        saveMenu = driver.find_element_by_xpath("//nav/div")
+        actions.move_to_element(saveMenu)
+        actions.context_click(saveMenu)
+        actions.perform()
+        actions.reset_actions()
+
+        saveButton = driver.find_element_by_xpath("//div[@id='context_1']/div[2]")
+
+        if SAVE_TICKET:
+            saveButton.click()
+            print("Ticket Submitted")
+        else:
+            print(
+                "Ticket not saved! Make sure to set the saveTicket variable to True if you want to actually submit changes to the ticket.")
 
 
 class Details:
@@ -53,56 +115,68 @@ class Details:
     def get_task_name(self):
         return self.taskName
 
-class CatalogTask:
-    notesTab = None
-    detailsTab = None
-    variablesTab = None
 
-    def __init__(self):
+class Notes:
+
+    def setAdditionalComments(self, commentString):
         switchToDefaultFrame()
         switchToContentFrame()
-        self.notesTab = Notes()
-        self.detailsTab = Details()
-        self.number = driver.find_element_by_id("sc_task.number").get_attribute("value")
-        self.requestItem = driver.find_element_by_id("sys_display.sc_task.request_item").get_attribute("value")
-        self.shortDescription = driver.find_element_by_id("sc_task.short_description").get_attribute("value")
-        self.state = select.Select(driver.find_element_by_id('sc_task.state'))
-        self.assignedTo = driver.find_element_by_id('sys_display.sc_task.assigned_to').get_attribute("value")
-
-    def get_state(self):
-        return self.state
-
-    def set_state(self, state):
-        print("Setting ticket state to " + state)
-        driver.find_element_by_id('sc_task.state').find_element_by_xpath("//option[. = '" + state + "']").click()
-        self.state = state
-
-#For Catalog Tasks of type dm_restock
-class DMRestockVariables:
-    def __init__(self):
-        self.restock_decom_repair = select.Select(driver.find_element_by_xpath("//tr[5]/td/div/div/div/div[2]/select"))
-
-    def set_restock_decom_repair(self, value):
+        print("Typing " + str(commentString))
+        commentArea = driver.find_element_by_xpath("//textarea[@id='activity-stream-comments-textarea']")
+        # Sometimes the comment box area is collapsed by default, so we must click the button to expand it first
         try:
-            self.restock_decom_repair.select_by_value(value)
+            commentArea.send_keys(commentString)
         except seleniumExceptions.ElementNotInteractableException:
-            driver.find_element_by_xpath("//span[3]/span/nav/div/div[2]/span/button").click()
-            self.restock_decom_repair.select_by_value(value)
-
+            driver.find_element_by_xpath("//span[2]/span/nav/div/div[2]/span/button").click()
+            commentArea.send_keys(commentString)
 
 #For Catalog Tasks of type dm_repair
-class DMRepairVariables:
+class RepairVar:
     pass
+
+class RepairTask(CatalogTask):
+    #Stuff for repair
+    pass
+
+
+#For Catalog Tasks of type dm_restock
+class RestockVar:
+    action_required = None
+    field_tech = None
+    assigned_group = None
+    staging_location = None
+
+    computer_name = None
+    asset_tag = None
+    restock_decom_repair = None
+    complete_at = None #ISC or MDC
+
+    def __init__(self):
+        try: #Try to get the action selection menu
+            self.restock_decom_repair = select.Select(driver.find_element_by_xpath("//tr[5]/td/div/div/div/div[2]/select"))
+        # If we can't get the menu, try expanding the parent menu out
+        except seleniumExceptions.ElementNotInteractableException:
+            driver.find_element_by_xpath("//span[3]/span/nav/div/div[2]/span/button").click()
+            self.restock_decom_repair = select.Select(driver.find_element_by_xpath("//tr[5]/td/div/div/div/div[2]/select"))
+
+    #Valid values are "Restock" "Repair" and "Decommission"
+    def select_restock_decom_repair(self, value):
+            self.restock_decom_repair.select_by_value(value)
+
+
+    def select_complete_at(self, value):
+        dropdown = select.Select(driver.find_element_by_xpath("//tr[3]/td/div/div/div/div[2]/select"))
+
+        if value == "ISC":
+           dropdown.select_by_value("Yes")
+        elif value == "MDC":
+            dropdown.select_by_value("No")
 
 
 class RestockTask(CatalogTask):
     def __init__(self):
         CatalogTask.__init__(self)
-        self.variablesTab = DMRestockVariables()
-
-class RepairTask(CatalogTask):
-    #Stuff for repair
-    pass
+        self.variablesTab = RestockVar()
 
 
 class ServiceNow:
@@ -117,6 +191,7 @@ class ServiceNow:
     @staticmethod
     def homepage():
         driver.get("https://prismahealth.service-now.com")
+
 
 
 class Table:
@@ -138,66 +213,73 @@ class Table:
         self.rows_body = self.table_body.find_elements_by_tag_name("tr")
         self.rows_head = self.table_head.find_elements_by_tag_name("tr")
 
-    def get_row_len_body(self):
+    def get_body_row_len(self):
         return len(self.rows_body)
 
-    def get_row_len_head(self):
+    def get_header_row_len(self):
         return len(self.rows_head)
 
-    def get_col_len_body(self):
+    def get_body_col_len(self):
         return len(self.rows_body[0].find_elements_by_tag_name('td')) - 1
 
-    def get_col_len_head(self):
+    def get_header_col_len(self):
         return len(self.rows_head[0].find_elements_by_tag_name('th')) - 1
 
-    def get_row_body(self, r):
+    def get_body_row(self, r):
         row_items = self.rows_body[r].find_elements_by_tag_name("td")
         #print("Getting row " + str(r))
         #for item in row_items:
             #print(item.text)
         return row_items
 
-    def get_row_head(self, r):
+    def get_header_row(self, r):
         row_items = self.rows_head[r].find_elements_by_tag_name("th")
         #print("Getting row " + str(r))
         #for item in row_items:
             #print(item.get_attribute("glide_label"))
         return row_items
 
-    def get_cell_body(self, r, c):
-        cell = self.get_row_body(r)[c]
+    def get_body_cell(self, r, c):
+        cell = self.get_body_row(r)[c]
         #print("row:" + str(r) + " col:" + str(c) + " is " + cell.text)
         return cell
 
-    def get_cell_head(self, r, c):
-        cell = self.get_row_head(r)[c]
+    def get_header_cell(self, r, c):
+        cell = self.get_header_row(r)[c]
         #print("row:" + str(r) + " col:" + str(c) + " is " + cell.get_attribute("glide_label"))
         return cell
 
     def get_col_name(self, c) -> str:
-        return str(self.get_cell_head(0, c).get_attribute("glide_label"))
+        return str(self.get_header_cell(0, c).get_attribute("glide_label"))
 
-    def get_col_by_name(self, name):
-        for i in range(self.get_col_len_head()):
+    def get_col_by_name(self, name) -> int:
+        for i in range(self.get_header_col_len()):
             if self.get_col_name(i).lower() == name.lower():
                 return i
 
-    def get_cell_by_column_and_name(self, colName, cellName):
-        for row in range(self.get_row_len_body()):
+# Returns the row and col position of cell, not the cell itself!
+    def get_cell_pos_by_col(self, colName, cellName) -> list:
+        for row in range(self.get_body_row_len()):
             col = self.get_col_by_name(colName)
-            cell = self.get_cell_body(row, col)
+            cell = self.get_body_cell(row, col)
             print(str(row) + "," +str(col) + " " + cell.text)
 
             if str(cell.text).lower() == cellName.lower():
-                print("FOUND CELL MATCHING CRITERIA [column=" + colName + " text=" + cellName + "]")
-                print(cell)
-                return cell
+                print("FOUND CELL!")
+                #print(cell)
+                return [row, col]
 
+
+#test csv
+csv = CSV.import_csv('input.csv')
+print(csv[0][1])
 
 ServiceNow.homepage()
 
 #Login
 driver.find_element_by_xpath("//*[@id='maincontent']/tbody/tr[4]/td[2]").click()
+
+
 
 #Go to a task
 #driver.get("https://prismahealth.service-now.com/nav_to.do?uri=task.do?sys_id=b537bb9e1bbff308f15a4005bd4bcb8b")
@@ -208,14 +290,26 @@ ServiceNow.tickets_assigned_to_tim()
 
 tims_table = Table("task_table")
 
-#Find cell with task SCTASK0031734
-tims_table.get_cell_by_column_and_name("Number","SCTASK0031734").click()
+#Find task of DT2UA4501XVD
+item_pos = tims_table.get_cell_pos_by_col("Configuration item", "DT2UA4501XVD")
+taskCol = tims_table.get_col_by_name("number")
+tims_table.get_body_cell(item_pos[0], taskCol).click()
 
-driver.get("https://prismahealth.service-now.com/nav_to.do?uri=%2Fsc_task.do%3Fsys_id%3Df59190fc1bc48410f15a4005bd4bcb9b%26sysparm_view%3Dtext_search")
+
+#driver.get("https://prismahealth.service-now.com/nav_to.do?uri=%2Fsc_task.do%3Fsys_id%3Df59190fc1bc48410f15a4005bd4bcb9b%26sysparm_view%3Dtext_search")
+
+
 
 dm_restock = RestockTask()
+dm_restock.set_state(__VALID_STATES['wip'])
+dm_restock.notes_tab.setAdditionalComments(__CANNED_RESPONSES["acknowledge"])
+dm_restock.submit()
 
-dm_restock.notesTab.setAdditionalComments("This is a test")
+dm_restock.set_state(__VALID_STATES['cc'])
+dm_restock.notes_tab.setAdditionalComments(__CANNED_RESPONSES['restock'])
+dm_restock.variablesTab.select_restock_decom_repair("restock")
+dm_restock.submit()
+
 
 #task = CatalogTask()
 #print(str(task.get_state()))
